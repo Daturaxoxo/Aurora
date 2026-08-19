33556d5a1ad7d92eeb94be7b8a2e856574201856
Daturaxoxo <luamoonzy@gmail.com>
2026-08-19 22:09:33 +1200

    Fix CHKSUM typo, disable broken addons, bump version

--- crates/shared/src/classes/info/addons.rs
+++ crates/shared/src/classes/info/addons.rs
@@ -1,8 +1,8 @@
 use super::version::Version;
 const UNAVAILABLE: &[(&str, &[Version])] = &[
-    ("drv_lin", &[Version::CN]),
-    ("col_tim", &[Version::CN]),
-    ("collectibles", &[Version::CN]),
+    ("drv_lin", &[Version::CN, Version::Global]),
+    ("col_tim", &[Version::CN, Version::Global]),
+    ("collectibles", &[Version::CN, Version::Global]),
 ];
 
 pub fn is_unavailable(config_key: &str, version: Version) -> bool {
