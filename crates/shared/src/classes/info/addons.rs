2001596c32862d8dee105e27a639756d361d6179
Daturaxoxo <luamoonzy@gmail.com>
2026-08-19 22:23:43 +1200

    forgot to disable addons for taiwan

--- crates/shared/src/classes/info/addons.rs
+++ crates/shared/src/classes/info/addons.rs
@@ -1,8 +1,8 @@
 use super::version::Version;
 const UNAVAILABLE: &[(&str, &[Version])] = &[
-    ("drv_lin", &[Version::CN, Version::Global]),
-    ("col_tim", &[Version::CN, Version::Global]),
-    ("collectibles", &[Version::CN, Version::Global]),
+    ("drv_lin", &[Version::CN, Version::Global, Version::TW]),
+    ("col_tim", &[Version::CN, Version::Global, Version::TW]),
+    ("collectibles", &[Version::CN, Version::Global, Version::TW]),
 ];
 
 pub fn is_unavailable(config_key: &str, version: Version) -> bool {
