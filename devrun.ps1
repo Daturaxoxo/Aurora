195c6b8ea8879ceb1a7d32de000f04952a8c6a88
Alawapr <93432929+Alawapr@users.noreply.github.com>
2026-08-21 00:32:33 +0200

    Improve windows run script

--- devrun.ps1
+++ devrun.ps1
@@ -1,1 +1,12 @@
-cargo build -p Aurora; if ($LASTEXITCODE -eq 0) { gsudo .\target\debug\Aurora.exe }
+cargo build -p Aurora
+if ($LASTEXITCODE -eq 0) {
+    $exePath = ".\target\debug\Aurora.exe"
+
+    if (Get-Command gsudo -ErrorAction SilentlyContinue) {
+        gsudo $exePath
+    } elseif (Get-Command sudo -ErrorAction SilentlyContinue) {
+        sudo $exePath
+    } else {
+        Start-Process $exePath -Verb RunAs
+    }
+}
